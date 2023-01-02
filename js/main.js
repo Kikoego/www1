3980b2cafdcb75ed6aeb5a1afc78ef6889c42f49
Rox <sllim03@mai.ru>
2023-01-03 02:28:50 +0700

    Закончил (есть адаптив, но dark mode не делал)

--- js/main.js
+++ js/main.js
@@ -24,3 +24,23 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "flex";
     dots[slideIndex-1].className += " active";
 }
+
+// Слайдер
+
+function dropMenu() {
+    document.getElementById("dropDownContent").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.navButton')) {
+
+        let dropdowns = document.getElementsByClassName("dropContent");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
